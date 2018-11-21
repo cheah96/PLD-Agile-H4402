dcfe4fab0022d0b261833f2a160efbca30d81531
Paul Du <du.paul136@gmail.com>
2018-11-21 09:01:13 +0100

    Add maven structure (#2)

---
+++ src/main/java/fr/insa/lyon/pld/agile/App.java
@@ -0,0 +1,13 @@
+package fr.insa.lyon.pld.agile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
