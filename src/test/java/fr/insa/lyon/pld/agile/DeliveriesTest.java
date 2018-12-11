39e169c10bd57bc1382293fb5e49eb78a92da35b
Alexandre van Beurden <KiLlOrBe@users.noreply.github.com>
2018-12-11 22:29:28 +0100

    Update deliveries update (#82)

--- src/test/java/fr/insa/lyon/pld/agile/DeliveriesTest.java
+++ src/test/java/fr/insa/lyon/pld/agile/DeliveriesTest.java
@@ -1,27 +1,20 @@
 package fr.insa.lyon.pld.agile;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalTime;
-import java.util.List;
-
+import java.util.Collection;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
 import fr.insa.lyon.pld.agile.model.Delivery;
-import fr.insa.lyon.pld.agile.model.DeliveryMan;
 import fr.insa.lyon.pld.agile.model.Map;
-import fr.insa.lyon.pld.agile.model.Node;
-import fr.insa.lyon.pld.agile.model.Section;
 import fr.insa.lyon.pld.agile.xml.XMLParser;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DeliveriesTest {
@@ -41,18 +34,37 @@ public class DeliveriesTest {
         XMLParser.loadDeliveries(map, pathDeliveries);
 
         map.setDeliveryManCount(1);
-
-        for(Delivery delivery: map.getDeliveries().values()) {
-            map.assignDelivery(delivery,map.getDeliveryMen().get(0));
-        }
-
-        map.unassignDelivery(3,map.getDeliveryMen().get(0));
-
+        
+        Collection<Delivery> deliveriesColl = map.getDeliveries().values();
+        
+        Object[] deliveries = deliveriesColl.toArray();
+   
+        map.assignDelivery((Delivery)deliveries[4],map.getDeliveryMen().get(0));
+        map.assignDelivery((Delivery)deliveries[3],map.getDeliveryMen().get(0));
+        map.assignDelivery((Delivery)deliveries[0],map.getDeliveryMen().get(0));
+        map.assignDelivery((Delivery)deliveries[2],map.getDeliveryMen().get(0));
+        
+        Delivery del0 = map.getDeliveryMen().get(0).getDeliveries().get(0);
+        assertTrue(del0.getNode().getId()==((Delivery)deliveries[4]).getNode().getId());
+        
+        Delivery del1 = map.getDeliveryMen().get(0).getDeliveries().get(1);
+        assertTrue(del1.getNode().getId()==((Delivery)deliveries[3]).getNode().getId());
+        
+        Delivery del2 = map.getDeliveryMen().get(0).getDeliveries().get(2);
+        assertTrue(del2.getNode().getId()==((Delivery)deliveries[0]).getNode().getId());
+        
+        Delivery del3 = map.getDeliveryMen().get(0).getDeliveries().get(3);
+        assertTrue(del3.getNode().getId()==((Delivery)deliveries[2]).getNode().getId());
+        
+        map.unassignDelivery(del1);
+        assertTrue(del2.getNode().getId()==map.getDeliveryMen().get(0).getDeliveries().get(1).getNode().getId());
+        
+        map.assignDelivery(2, del1, map.getDeliveryMen().get(0));
+        assertTrue(del1.getNode().getId()==map.getDeliveryMen().get(0).getDeliveries().get(3).getNode().getId());
+        
         LocalTime last = LocalTime.of(0, 0, 1, 1);
         for(int i=0;i<map.getDeliveryMen().get(0).getRound().getItinerary().size();++i) {
             LocalTime curr = map.getDeliveryMen().get(0).getRound().getItinerary().get(i).getArrivalTime();
-            System.out.println(last);
-            System.out.println(curr);
             assertTrue(curr.compareTo(last)>0);
             last=curr;
         }
