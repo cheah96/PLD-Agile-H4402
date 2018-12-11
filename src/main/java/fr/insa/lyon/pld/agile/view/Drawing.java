6a2b74ac36af0036718dda6da156db3ed2b507ca
ttiaz <45128239+ttiaz@users.noreply.github.com>
2018-12-12 00:14:14 +0100

    Javadoc for drawings  (#90)
    * Changed max number of delivery men/women, fixed status bar height :)
    
    * Javadoc Drawing

--- src/main/java/fr/insa/lyon/pld/agile/view/Drawing.java
+++ src/main/java/fr/insa/lyon/pld/agile/view/Drawing.java
@@ -12,8 +12,14 @@ import java.awt.image.BufferedImage;
  */
 public class Drawing {
     
-    // https://stackoverflow.com/questions/3514158/how-do-you-clone-a-bufferedimage
+
+    /**
+     * Creates a copy of the image. //la description de function
+     * @param image the image to copy  //la description de parametre
+     * @return a copy of the original image //la description de retour
+     */
     public static BufferedImage copyImage(BufferedImage image) {
+        // https://stackoverflow.com/questions/3514158/how-do-you-clone-a-bufferedimage
         BufferedImage copy = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
         Graphics2D g = copy.createGraphics();
         g.drawImage(image, 0, 0, null);
@@ -21,22 +27,44 @@ public class Drawing {
         return copy;
     }
     
+    /**
+     * Gets the color for a delivery man  
+     * @param index the number of the delivery man
+     * @param count the total number of delivery men
+     * @return the corresponding color to this delivery man
+     */
     public static Color getColor(int index, int count) {
         return Color.getHSBColor((float) index / count, 1.0f, 1.0f);
     }
     
+    /**
+     * Creates an enlightened version of the given color
+     * @param color the color to enlighten
+     * @return the enlightened version of the color
+     */
     public static Color getColorBrighter(Color color) {
         float col[] = color.getRGBColorComponents(null);
         int r=(int)(col[0]*127), g=(int)(col[1]*127), b=(int)(col[2]*127);
         return new Color(r+128,g+128,b+128);
     }
     
+    /**
+     * Creates a darker version of the given color
+     * @param color the color to enlighten
+     * @return the darker version of the color
+     */
     public static Color getColorDarker(Color color) {
         float col[] = color.getRGBColorComponents(null);
         int r=(int)(col[0]*127), g=(int)(col[1]*127), b=(int)(col[2]*127);
         return new Color(r,g,b);
     }
     
+    /**
+     * draw a thick line between two points
+     * @param g the object to draw on
+     * @param p1 the first point  
+     * @param p2 the second point 
+     */
     protected static void drawLineThick(Graphics g, Point p1, Point p2) {
         for (int dx=-1; dx<=1; dx++) {
             for (int dy=-1; dy<=1; dy++) {
@@ -44,6 +72,15 @@ public class Drawing {
             }
         }
     }
+
+    /**
+     * displays the arrows on the line between two points
+     * @param g the object to draw on
+     * @param p1 the first point
+     * @param p2 the second point
+     * @param position distance before next arrow 
+     * @return the distance before the next arrow for the next line
+     */
     protected static double drawLineArrows(Graphics g, Point p1, Point p2, double position) {
         double step = 150.;
         
@@ -83,32 +120,73 @@ public class Drawing {
         return total; 
     }
     
+    /**
+     * gets the distance between two points
+     * @param p1 the first point
+     * @param p2 the second point
+     * @return the distance
+     */
     protected static double getPointsDistance(Point p1, Point p2) {
         int vx = p2.x - p1.x, vy = p2.y - p1.y;
         return getVectorLength(vx, vy);
     }
+
+    /**
+     * gets the length of the vector between two vectors
+     * @param vx the first vector
+     * @param vy the second vector
+     * @return the vector
+     */
     protected static double getVectorLength(int vx, int vy) {
         return Math.sqrt((double) ((vx*vx)+(vy*vy)));
     }
     
+    /**
+     * draws a line between two points
+     * @param g the object to be drawn on
+     * @param p1 the first point
+     * @param p2 the second point
+     */
     protected static void drawLine(Graphics g, Point p1, Point p2) {
         g.drawLine(p1.x, p1.y, p2.x, p2.y);
     }
     
+    /**
+     * draws a dot with a selected size on the chosen coordinates 
+     * @param g the object to be drawn on
+     * @param coords the coordinates of the points
+     * @param diameter the diameter for the dot drawn
+     */
     protected static void drawDot(Graphics g, Point coords, int diameter) {
         g.fillOval((int) coords.getX()-diameter/2, (int) coords.getY()-diameter/2, diameter, diameter);
     }
     
+    /**
+     * draws the house
+     * @param g the object to be drawn on
+     * @param coords the coordinates of the house
+     * @param size the size of the house
+     */
     protected static void drawHouse(Graphics g, Point coords, int size) {
         // g.fillPolygon(new int[] {coords.x-size/2, coords.x, coords.x+size/2}, new int[] {coords.y+size/2, coords.y+size, coords.y+size/2}, 3);
         g.fillRect(coords.x - size/2, coords.y - size/2, size, size);
     }
     
+    /**
+     * draws the warehouse
+     * @param g the object to be drawn on
+     * @param coords the coordinates of the warehouse
+     */
     protected static void drawWarehouse(Graphics g, Point coords) {
         g.setColor(Color.black);
         drawHouse(g, coords, 18);
     }
     
+    /**
+     * draws the deliveries
+     * @param g the object to be drawn on
+     * @param coords the coordinates of the deliveries
+     */
     protected static void drawDelivery(Graphics g, Point coords, Color color) {
         g.setColor(Drawing.getColorDarker(color));
         drawDot(g, coords, 11);
@@ -116,6 +194,12 @@ public class Drawing {
         drawDot(g, coords, 7);
     }
     
+    /**
+     * draws the selected node 
+     * @param g the object to be drawn on
+     * @param color the selected color of the node
+     * @param coords the coordinates of the nodes
+     */
     protected static void drawSelectedNode(Graphics g, Point coords, Color color) {
         g.setColor(Color.black);
         drawDot(g, coords, 15);
